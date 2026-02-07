c5e807ad90b8d5a8d255afd957eb052b27642d17
vinit-kaushik <kaushikvinit.09@gmail.com>
2026-02-07 12:41:17 +0530

    feat: gemini keys changed and ai runs fine

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -3,12 +3,12 @@ import {
     HarmCategory,
     HarmBlockThreshold,
   } from "@google/generative-ai"
-  
-  const apiKey = "AIzaSyA7BNwrDEsPTo7KM4NOFDPlCUpAqbBaRaY";
+
+  const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.5-flash",
   });
   
   const generationConfig = {
